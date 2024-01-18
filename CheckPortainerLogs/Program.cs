234515d0701c2d9ac0924d8c5f36fa0b046e4e4e
MrSatisfactionGuy <edd.wag18@gmail.com>
2024-01-18 18:18:10 +0100

    first commit

---
+++ CheckPortainerLogs/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+string dockeApiEndpoint = string.Empty;
+string containerName = string.Empty;
+string logsendpoint = $"{dockeApiEndpoint}/cotainers/{containerName}/logs";
+
+async Task<bool> GetHttpResponse()
+{
+    try
+    {
+        HttpClient client = new HttpClient();
+        HttpResponseMessage response = await client.GetAsync(dockeApiEndpoint);
+        string logs = await response.Content.ReadAsStringAsync();
+        Console.WriteLine(logs);
+    }
+    catch(Exception ex)
+    {
+        await Console.Out.WriteLineAsync(ex.Message);
+    }
+
+    return true;
+}
